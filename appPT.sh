a95148c85a99e650411547730b6d32e56e813074
Pech99 <vittorio1999@live.com>
2023-02-26 22:52:20 +0100

    app

--- appPT.sh
+++ appPT.sh
@@ -23,17 +23,18 @@ exit
 enable
 conf t
 
+#ACL
 ip access-list extended 110
 permit IP 192.168.1.0 0.0.0.255 any
 permit TCP any any established
 exit
-
 ip access-list extended 111
 permit IP 192.168.0.0 0.0.0.255 any
 permit TCP any any established
 permit TCP any host 192.168.1.1 eq www
 exit
 
+#interface
 interface Fa0/0. _NUNBER_
 encapsulation dot1Q _NUNBER_
 ip address _ADDRESS_ _MASK_
@@ -52,18 +53,21 @@ ip nat outside
 no shutdown
 exit
 
+#RIP
 router rip
 version 2
 network _ADDRESS_
 passive-interface Fa0/0
 exit
 
+#OSPF
 router ospf 1
 area 1 stub 
 network _ADDRESS_ _WILDCARD_ area 1
 passive-interface Fa0/0
 exit
 
+#Static & NAT
 ip route _ADDRESSTO_ _MASK_ _ADDRESSUSE_
 access-list 100 permit ip any any
 ip nat inside source list 100 interface Fa0/0
@@ -78,5 +82,4 @@ write mem
 exit
 exit
 exit
-
 
